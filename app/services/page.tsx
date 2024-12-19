c68ae88f88fd1b9132f2c62e5fad1e3cc85e3dc7
mdsimpson09 <131899003+mdsimpson09@users.noreply.github.com>
2024-12-18 22:54:11 -0500

    edits

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -37,23 +37,24 @@ export default function ServicesPage() {
       <main className="flex-grow py-16">
         <div className="max-w-6xl mx-auto px-4 space-y-16">
           <SpecialtyItem
-            title="Individual Therapy"
-            description="One-on-one therapy designed to help you navigate life’s challenges, explore your emotions, and build tools for personal growth. Offered virtually or in person to suit your needs."
-            imageSrc="/individual-therapy.jpg" // Replace with your image
+            title="Free Consultation"
+            description="A no-cost, 15-minute consultation to discuss your needs, answer questions, and determine if we’re the right fit for your journey."
+            imageSrc="/priscilla-du-preez.jpg" // Replace with your image
             reverse={false}
           />
           <SpecialtyItem
-            title="EcoTherapy"
-            description="Reconnect with nature to support emotional well-being and reduce stress. Available as outdoor sessions or virtually with nature-focused mindfulness techniques."
-            imageSrc="/ecotherapy.jpg" // Replace with your image
+            title="Individual Therapy"
+            description="One-on-one therapy designed to help you navigate life’s challenges, explore your emotions, and build tools for personal growth. Offered virtually."
+            imageSrc="/lauren-mancke.jpg" // Replace with your image
             reverse={true}
           />
           <SpecialtyItem
-            title="Free Consultation"
-            description="A no-cost, 15-minute consultation to discuss your needs, answer questions, and determine if we’re the right fit for your journey."
-            imageSrc="/consultation.jpg" // Replace with your image
+            title="EcoTherapy"
+            description="Reconnect with nature to support emotional well-being and reduce stress. Available virtually. In-person sessions coming soon."
+            imageSrc="/lala-azizli.jpg" // Replace with your image
             reverse={false}
           />
+          
         </div>
       </main>
 
@@ -82,8 +83,8 @@ const SpecialtyItem = ({
     {/* Image */}
     <div className="w-full md:w-1/2">
       <Image
-        src={imageSrc}
-        alt={title}
+        src= {imageSrc}
+        alt='work space'
         width={600}
         height={400}
         className="rounded-lg shadow-md"
