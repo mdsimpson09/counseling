175d798d9c0be8415b7c33594a77c68bec4dc927
mdsimpson09 <131899003+mdsimpson09@users.noreply.github.com>
2025-02-05 17:58:02 -0800

    google tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,8 @@ export const metadata: Metadata = {
   description: "Simpson Counseling",
 };
 
+const GTM_ID = process.env.NEXT_PUBLIC_GTM_ID || "GTM-K9JJDHZL"; // Fallback if env is not set
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -25,9 +27,32 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','${GTM_ID}');
+            `,
+          }}
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        {/* Google Tag Manager (noscript) */}
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `
+              <iframe src="https://www.googletagmanager.com/ns.html?id=${GTM_ID}"
+              height="0" width="0" style="display:none;visibility:hidden"></iframe>
+            `,
+          }}
+        />
         {children}
       </body>
     </html>
